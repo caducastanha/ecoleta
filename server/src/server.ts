a88ee0c13b736746ce0d4515a2c07fa5e81f10ad
Cadu Castanha <caducastanha@gmail.com>
2020-06-02 23:48:11 -0300

    criando as rotas da aplicação

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import { resolve } from 'path'
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-  return res.json([
-    'Cadu',
-    'Lari',
-    'Mônica'
-  ]);
-});
+app.use(cors())
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')))
 
 app.listen(3333);
